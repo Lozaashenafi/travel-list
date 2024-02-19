df2f94c7766836f478bb6bcab12a688ffdc74e1b
Lozaashenafi <134071796+Lozaashenafi@users.noreply.github.com>
2024-02-19 19:16:48 +0300

    my first react

---
+++ src/components/Form.js
@@ -0,0 +1,38 @@
+import { useState } from "react";
+
+export default function Form({ onAddItems }) {
+  const [description, setDescription] = useState("");
+  const [quantitys, setQuantitys] = useState(1);
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    if (!description) return;
+
+    const newItem = { description, quantitys, packed: false, id: Date.now() };
+    console.log(newItem);
+
+    onAddItems(newItem);
+    setDescription("");
+    setQuantitys(1);
+  }
+  return (
+    <form className="add-form" onSubmit={handleSubmit}>
+      <h2>What do you need for your 😍 trip?</h2>
+      <select value={quantitys} onChange={(e) => setQuantitys(e.target.value)}>
+        {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
+          <option value={num} key={num}>
+            {num}
+          </option>
+        ))}
+      </select>
+      <input
+        type="text"
+        placeholder="Item..."
+        value={description}
+        onChange={(e) => setDescription(e.target.value)}
+      />
+      <button>Add</button>
+    </form>
+  );
+}
